db7a41954ff5d34be4bb2a38e88e067e84996615
Juanmagos <juanmagoss@hotmail.com>
2018-02-26 09:35:29 -0600

    more places

--- bin/seeds.js
+++ bin/seeds.js
@@ -14,6 +14,36 @@ const places = [
   pricing:[{regular: 0}],
   participants: [],
   picPath: "/images/patanegra.jpg"
+  },
+
+  { name: "Articbar",
+  location: { type: "Point", coordinates: [ -99.171981, 19.412736] },
+  description: "It's an antro",
+  reviews:[{rating: 5, crowded: 2, music: 1, creatorId: 10155234846446892}],
+  type: "Salon",
+  pricing:[{regular: 0}],
+  participants: [],
+  picPath: "/images/articbar.png"
+  },
+
+  { name: "Mama Rumba",
+  location: { type: "Point", coordinates: [ -99.164944, 19.413961] },
+  description: "It's an antro",
+  reviews:[{rating: 5, crowded: 4, music: 4, creatorId: 10155234846446892}],
+  type: "Salon",
+  pricing:[{regular: 0}],
+  participants: [],
+  picPath: "/images/MamaRumba.jpg"
+  },
+
+  { name: "Bulldog Cafe",
+  location: { type: "Point", coordinates: [  -99.186903, 19.378694] },
+  description: "It's an antro",
+  reviews:[{rating: 5, crowded: 4, music: 4, creatorId: 10155234846446892}],
+  type: "Salon",
+  pricing:[{regular: 0}],
+  participants: [],
+  picPath: "/images/Bulldog cafe.jpg"
   }
 ];
 
